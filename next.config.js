cae369ccb9c21e613885c6e74b9d54ff2a43071f
Ovodo <ovdizzle@gmail.com>
2023-06-26 21:36:55 +0100

    deleted extra index.js

--- next.config.js
+++ next.config.js
@@ -1,30 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  // output: "out",
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "s3-alpha-sig.figma.com",
-        port: "",
-        pathname: "/img/**",
-      },
-      {
-        protocol: "https",
-        hostname: "scontent.fabv2-2.fna.fbcdn.net",
-        port: "",
-        pathname: "/v/**",
-      },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-        port: "",
-        pathname: "/u/**",
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
