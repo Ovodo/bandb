ab68b4e99d52097eb71d83175f594274f3388b8a
Ovodo <ovdizzle@gmail.com>
2023-07-18 23:15:25 +0100

    Deleted Meterguage Import

--- Components/LongCard.jsx
+++ Components/LongCard.jsx
@@ -4,15 +4,26 @@ export default function LongCard({ title }) {
   const text1 = (
     <>
       <br />
-      The Bear and Bull Index provides an unbiased view of the crypto market, backed by{" "}
-      <a href="https://lunarcrush.com/" target="_blank" rel="noopener noreferrer">
+      The Bear and Bull Index provides an unbiased view of the crypto market,
+      backed by{" "}
+      <a
+        href='https://lunarcrush.com/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
         LunarCrush’s
       </a>{" "}
-      AI-driven social insights and metrics. We help the average crypto user to better understand the market through technical analysis and social analysis made easy.
+      AI-driven social insights and metrics. We help the average crypto user to
+      better understand the market through technical analysis and social
+      analysis made easy.
       <br />
       <br />
       <b>Learn More:</b> Check out the{" "}
-      <a href="https://docs.google.com/document/d/1dsthZ5d2hSGMdHRsdGo_qVAlc7XmtzT9mtwtiea6Zdk/edit" target="_blank" rel="noopener noreferrer">
+      <a
+        href='https://docs.google.com/document/d/1dsthZ5d2hSGMdHRsdGo_qVAlc7XmtzT9mtwtiea6Zdk/edit'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
         <b>whitepaper</b>
       </a>
     </>
@@ -21,7 +32,8 @@ export default function LongCard({ title }) {
   const text2 = (
     <>
       <br />
-      We gather data from the following sources every day to visualize social sentiment changes in the crypto market.
+      We gather data from the following sources every day to visualize social
+      sentiment changes in the crypto market.
       <br />
       <br />
       <b>Key metrics on LunarCrush we analyze for insight:</b>
@@ -49,13 +61,16 @@ export default function LongCard({ title }) {
   const text3 = (
     <>
       <br />
-      The following built-in indicators help visualize social and technical analysis sentiment changes in the crypto market.
+      The following built-in indicators help visualize social and technical
+      analysis sentiment changes in the crypto market.
       <br />
       <br />
       <b>1. Market Sentiment Analysis</b>
       <br />
       <br />
-      The Market Sentiment Analysis provides a general overview of the crypto market, and the indicator ranges between Bearish, Neural, and Bullish sentiment:
+      The Market Sentiment Analysis provides a general overview of the crypto
+      market, and the indicator ranges between Bearish, Neural, and Bullish
+      sentiment:
       <br />
       <br />
       <ol>
@@ -68,26 +83,42 @@ export default function LongCard({ title }) {
       <b>2. Social Analysis Summary</b>
       <br />
       <br />
-      The Social Analysis Summary analyzes what social media platforms are saying about the crypto market. The indicator ranges between Sell, Neutral, or Buy:
+      The Social Analysis Summary analyzes what social media platforms are
+      saying about the crypto market. The indicator ranges between Sell,
+      Neutral, or Buy:
       <br />
       <br />
       <ol>
-        <li>• Values of 60 or above indicate a Buy or Dollar cost average sentiment.</li>
-        <li>• Values between 40 and 59 indicate a Neutral or No opinion sentiment.</li>
-        <li>• Values between 0 and 39 indicate a Sell or Use stop loss sentiment.</li>
+        <li>
+          • Values of 60 or above indicate a Buy or Dollar cost average
+          sentiment.
+        </li>
+        <li>
+          • Values between 40 and 59 indicate a Neutral or No opinion sentiment.
+        </li>
+        <li>
+          • Values between 0 and 39 indicate a Sell or Use stop loss sentiment.
+        </li>
       </ol>
       <br />
       <br />
       <b>3. BTD and STP Indicator</b>
       <br />
       <br />
-      The BTD (Buy the Dip) and STP (Sell the Pump) indicators are simplified indicators created from our research. They are time-sensitive and change quickly with crypto market sentiment. To track the market sentiment, we created the Market Sentiment Indicator to complement the BTD and STP indicators.
+      The BTD (Buy the Dip) and STP (Sell the Pump) indicators are simplified
+      indicators created from our research. They are time-sensitive and change
+      quickly with crypto market sentiment. To track the market sentiment, we
+      created the Market Sentiment Indicator to complement the BTD and STP
+      indicators.
       <br />
       <br />
       <b>4. Crypto Market Sentiment Indicator</b>
       <br />
       <br />
-      The crypto market sentiment indicator measures when the crypto market is fearful or hopeful. However, like the BTD (Buy the Dip) and STP (Sell the Pump) indicator, it is time-sensitive and changes quickly with the general crypto market sentiment driven by social media.
+      The crypto market sentiment indicator measures when the crypto market is
+      fearful or hopeful. However, like the BTD (Buy the Dip) and STP (Sell the
+      Pump) indicator, it is time-sensitive and changes quickly with the general
+      crypto market sentiment driven by social media.
     </>
   );
 
@@ -97,27 +128,39 @@ export default function LongCard({ title }) {
       <b> No Investment Advice Here, Please take note of the following:</b>
       <br />
       <br />
-      <b>1.</b> The BandBindex App is for educational purposes only. It provides a general view of the market, but complementing it with other metrics and indicators will give you a more balanced understanding.
+      <b>1.</b> The BandBindex App is for educational purposes only. It provides
+      a general view of the market, but complementing it with other metrics and
+      indicators will give you a more balanced understanding.
       <br />
       <br />
-      <b>2.</b> The information provided on this website does not constitute investment advice, financial advice, trading advice, or any other form of advice. You should not treat any of the website's content as such.
+      <b>2.</b> The information provided on this website does not constitute
+      investment advice, financial advice, trading advice, or any other form of
+      advice. You should not treat any of the website's content as such.
       <br />
       <br />
-      <b>3.</b> We do not recommend buying, selling, or holding any cryptocurrency. Please conduct your own due diligence and consult your financial advisor before making any investment decisions.
+      <b>3.</b> We do not recommend buying, selling, or holding any
+      cryptocurrency. Please conduct your own due diligence and consult your
+      financial advisor before making any investment decisions.
       <br />
       <br />
       <b>4.</b> The{" "}
-      <a href="https://docs.google.com/document/d/1dsthZ5d2hSGMdHRsdGo_qVAlc7XmtzT9mtwtiea6Zdk/edit" target="_blank" rel="noopener noreferrer">
+      <a
+        href='https://docs.google.com/document/d/1dsthZ5d2hSGMdHRsdGo_qVAlc7XmtzT9mtwtiea6Zdk/edit'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
         <b>Bear and Bull $INDEX token</b>
       </a>{" "}
-      is not a currency or an investment of any form. It is an In-App reward token created to incentivize community participation and unlock features within the BandBindex App and community.
+      is not a currency or an investment of any form. It is an In-App reward
+      token created to incentivize community participation and unlock features
+      within the BandBindex App and community.
     </>
   );
 
   return (
-    <div className="bg-white shadow-md p-5 w-[80vw] md:w-[87vw] lg:w-[92%] rounded-md">
-      <h1 className="text-2xl font-bold mb-4">{title}</h1>
-      <p className="text-lg">
+    <div className='bg-white shadow-md p-5 w-[80vw] md:w-[87vw] lg:w-[92%] rounded-md'>
+      <h1 className='text-2xl font-bold mb-4'>{title}</h1>
+      <p className='text-lg'>
         {title === "Why Bear & Bull Index?"
           ? text1
           : title === "Data Sources"
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import InsightsCard from "@/Components/InsightsCard";
 import SemiCircle from "@/Components/SemiCircle";
 import SemiCircle1 from "@/Components/SemiCircle1";
 import SemiCircle2 from "@/Components/SemiCircle2";
-import MeterGauge from "@/Components/MeterGauge";
+// import MeterGauge from "@/Components/MeterGauge";
 import Charts from "@/Components/Charts";
 import MarketSentiment from "@/Components/MarketSentiment";
 import CountdownTimer from "@/components/CountdownTimer";
@@ -73,24 +73,27 @@ export default function Home({
     return (
       outputStart +
       ((outputEnd - outputStart) * (input - inputStart)) /
-      (inputEnd - inputStart)
+        (inputEnd - inputStart)
     );
   }
 
   console.log(BTD);
 
   return (
-    <main className="relative flex flex-col">
-      <div className="flex w-full justify-between h-[11vh] md:h-[14vh] lg:h-[18vh]">
-        <div className="flex relative bottom-[1vh] lg:bottom-0 h-[70%] my-auto self-start items-center">
-          <h1 className="absolute top-[2vh] md:top-[1vh] ml-[6vh] md:ml-[9.5vw] lg:ml-[8vw] min-w-max self-center">
+    <main className='relative flex flex-col'>
+      <div className='flex w-full justify-between h-[11vh] md:h-[14vh] lg:h-[18vh]'>
+        <div className='flex relative bottom-[1vh] lg:bottom-0 h-[70%] my-auto self-start items-center'>
+          <h1 className='absolute top-[2vh] md:top-[1vh] ml-[6vh] md:ml-[9.5vw] lg:ml-[8vw] min-w-max self-center'>
             {mobile ? "" : "📉 Bear & Bull Index"}
           </h1>
         </div>
-        <h4 className="self-end mx-auto">Market Overview</h4>
-        <div className="w-[20vw] self-center absolute items-center right-[2vw] flex flex-col justify-center">
-          <h4 className="hidden font-[800] lg:flex">{todaysDate}</h4>
-          <p style={{ lineHeight: 1 }} className="font-[900] text-center text-orange-500 text-lg">
+        <h4 className='self-end mx-auto'>Market Overview</h4>
+        <div className='w-[20vw] self-center absolute items-center right-[2vw] flex flex-col justify-center'>
+          <h4 className='hidden font-[800] lg:flex'>{todaysDate}</h4>
+          <p
+            style={{ lineHeight: 1 }}
+            className='font-[900] text-center text-orange-500 text-lg'
+          >
             {mobile ? "" : <CountdownTimer />}
           </p>
           <b>
@@ -98,27 +101,35 @@ export default function Home({
           </b>
         </div>
       </div>
-      <section className="mx-[2vw] flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 mt-[4vh]">
-        <InsightsCard text="Historical Analysis">
-          <div className="flex w-[80vw] h-full justify-center items-center">
+      <section className='mx-[2vw] flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 mt-[4vh]'>
+        <InsightsCard text='Historical Analysis'>
+          <div className='flex w-[80vw] h-full justify-center items-center'>
             <Charts height={mobile ? 430 : 280} chartData={chartData} />
           </div>
         </InsightsCard>
-        <InsightsCard text="Actionable Insight">
-          <div className="flex flex-col justify-between relative bottom-[2vh] items-center self-center h-[85%]">
-            <div className="bg-gray-700 relative top-[10vh] w-[65%] h-[12%]">
-              <div style={{ width: `${BTD}%` }} className="bg-[#04bd64ff] w-[30%] h-full"></div>
+        <InsightsCard text='Actionable Insight'>
+          <div className='flex flex-col justify-between relative bottom-[2vh] items-center self-center h-[85%]'>
+            <div className='bg-gray-700 relative top-[10vh] w-[65%] h-[12%]'>
+              <div
+                style={{ width: `${BTD}%` }}
+                className='bg-[#04bd64ff] w-[30%] h-full'
+              ></div>
             </div>
-            <p className="self-center relative top-[5vh] text-[20px] font-[800] text-[#04bd64ff]">
+            <p className='self-center relative top-[5vh] text-[20px] font-[800] text-[#04bd64ff]'>
               Buy the Dip
             </p>
-            <div className="bg-gray-700 relative top-[5vh] w-[65%] h-[12%]">
-              <div style={{ width: `${STP}%` }} className="bg-[#c0041dff] w-[30%] h-full"></div>
+            <div className='bg-gray-700 relative top-[5vh] w-[65%] h-[12%]'>
+              <div
+                style={{ width: `${STP}%` }}
+                className='bg-[#c0041dff] w-[30%] h-full'
+              ></div>
             </div>
-            <p className="self-center text-[20px] font-[800] text-[#c0041dff]">Sell the Pump</p>
+            <p className='self-center text-[20px] font-[800] text-[#c0041dff]'>
+              Sell the Pump
+            </p>
           </div>
         </InsightsCard>
-        <InsightsCard text="Market Sentiment">
+        <InsightsCard text='Market Sentiment'>
           <MarketSentiment sentiment={sentiment} />
         </InsightsCard>
         <InsightsCard
@@ -127,9 +138,9 @@ export default function Home({
           lastweek={lastweek.MSA}
           lastMonth={lastMonth.MSA}
           insights
-          text="Market Sentiment Analysis"
+          text='Market Sentiment Analysis'
         >
-          <div className="relative bottom-7">
+          <div className='relative bottom-7'>
             <SemiCircle guage={lerp(0, 100, -90, 90, today.MSA)} />
           </div>
         </InsightsCard>
@@ -139,9 +150,9 @@ export default function Home({
           lastweek={lastweek.SAS}
           lastMonth={lastMonth.SAS}
           insights
-          text="Social Analysis Summary"
+          text='Social Analysis Summary'
         >
-          <div className="relative bottom-7">
+          <div className='relative bottom-7'>
             <SemiCircle1 guage={lerp(0, 100, -90, 90, today.SAS)} />
           </div>
         </InsightsCard>
@@ -151,26 +162,35 @@ export default function Home({
           lastweek={lastweek.RSI}
           lastMonth={lastMonth.RSI}
           insights
-          text="Relative Strength Index"
+          text='Relative Strength Index'
         >
-          <div className="relative bottom-7">
+          <div className='relative bottom-7'>
             <SemiCircle2 guage={lerp(0, 100, -90, 90, today.RSI)} />
           </div>
         </InsightsCard>
       </section>
-      <section className="flex flex-col mb-[15vh] items-center">
-        <LongCard title="Why Bear & Bull Index?" text={text1} />
+      <section className='flex flex-col mb-[15vh] items-center'>
+        <LongCard title='Why Bear & Bull Index?' text={text1} />
       </section>
-      <section className="flex flex-col mb-[15vh] items-center">
-        <LongCard title="Data Sources" text={text2} />
+      <section className='flex flex-col mb-[15vh] items-center'>
+        <LongCard title='Data Sources' text={text2} />
       </section>
-      <section className="flex flex-col mb-[15vh] items-center">
-        <LongCard title="Bear and Bull Index indicators" text={text3} />
+      <section className='flex flex-col mb-[15vh] items-center'>
+        <LongCard title='Bear and Bull Index indicators' text={text3} />
       </section>
-      <section className="flex flex-col mb-[15vh] items-center">
-        <LongCard title="Disclaimer" text={text4} />
+      <section className='flex flex-col mb-[15vh] items-center'>
+        <LongCard title='Disclaimer' text={text4} />
       </section>
-      <center>Bear and Bull Index is Powered by <a href="https://lunarcrush.com/" target="_blank" rel="noopener noreferrer"><b>LunarCrush</b></a></center>
+      <center>
+        Bear and Bull Index is Powered by{" "}
+        <a
+          href='https://lunarcrush.com/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <b>LunarCrush</b>
+        </a>
+      </center>
       <br />
       <br />
     </main>
