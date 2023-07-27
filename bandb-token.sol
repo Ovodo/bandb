e14fc746d974043088c67475b1925f6bda482a0d
Rob <38807144+LiquidityApe@users.noreply.github.com>
2023-07-27 01:08:01 +0100

    Update INDEX Token to INDEX

--- bandb-token.sol
+++ bandb-token.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract INDEX is ERC20 {
     // Constructor sets the initial total supply and assigns it to the contract deployer
-    constructor() ERC20("INDEX Token", "INDEX") {
+    constructor() ERC20("INDEX", "INDEX") {
         uint256 maxSupply = 21 * 10**6 * 10**18; // 21 million tokens with 18 decimal places
         _mint(msg.sender, maxSupply);
     }
