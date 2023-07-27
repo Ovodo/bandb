0f8c2994e5620ec3bf4db18a5565d53d7c15246a
Rob <38807144+LiquidityApe@users.noreply.github.com>
2023-07-27 02:06:54 +0100

    Added Pausable contract directly

--- bandb-token.sol
+++ bandb-token.sol
@@ -2,11 +2,10 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract BandB is ERC20Burnable, ERC20Pausable, AccessControl {
+contract BandB is ERC20Burnable, AccessControl, Pausable {
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     uint256 public constant MAX_SUPPLY = 21 * 10**6 * 10**18; // 21 million tokens with 18 decimal places
 
@@ -28,6 +27,5 @@ contract BandB is ERC20Burnable, ERC20Pausable, AccessControl {
 
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
         super._beforeTokenTransfer(from, to, amount);
-        ERC20Pausable._beforeTokenTransfer(from, to, amount);
     }
 }
