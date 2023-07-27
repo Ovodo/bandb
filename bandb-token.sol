db829d265fe64780a2b1bdcb2c97ad79113b69ff
Rob <38807144+LiquidityApe@users.noreply.github.com>
2023-07-27 01:00:18 +0100

    Create bandb-token.sol

---
+++ bandb-token.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract INDEX is ERC20 {
+    // Constructor sets the initial total supply and assigns it to the contract deployer
+    constructor() ERC20("INDEX Token", "INDEX") {
+        uint256 maxSupply = 21 * 10**6 * 10**18; // 21 million tokens with 18 decimal places
+        _mint(msg.sender, maxSupply);
+    }
+}
