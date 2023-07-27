06084027269d52f796420324f2b0d4fd711fdb05
Rob <38807144+LiquidityApe@users.noreply.github.com>
2023-07-27 01:51:14 +0100

    Added pauser role but no minter function

--- bandb-token.sol
+++ bandb-token.sol
@@ -1,12 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract INDEX is ERC20 {
-    // Constructor sets the initial total supply and assigns it to the contract deployer
-    constructor() ERC20("INDEX", "INDEX") {
-        uint256 maxSupply = 21 * 10**6 * 10**18; // 21 million tokens with 18 decimal places
-        _mint(msg.sender, maxSupply);
+contract BandB is ERC20Burnable, ERC20Pausable, AccessControl {
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    uint256 public constant MAX_SUPPLY = 21 * 10**6 * 10**18; // 21 million tokens with 18 decimal places
+
+    constructor() ERC20("BandB", "INDEX") {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(PAUSER_ROLE, msg.sender);
+        _mint(msg.sender, MAX_SUPPLY);
+    }
+
+    function pause() public {
+        require(hasRole(PAUSER_ROLE, msg.sender), "Must have pauser role to pause");
+        _pause();
+    }
+
+    function unpause() public {
+        require(hasRole(PAUSER_ROLE, msg.sender), "Must have pauser role to unpause");
+        _unpause();
     }
 }
