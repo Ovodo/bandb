d6a8182a49a24b0f1564e3b74157861a1a5e411e
Rob <38807144+LiquidityApe@users.noreply.github.com>
2023-07-27 02:14:58 +0100

    Delete CustomPausable.sol

--- CustomPausable.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/security/Pausable.sol";
-
-abstract contract CustomPausable is Pausable {
-    // Empty override to avoid conflict with ERC20's _beforeTokenTransfer function
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {}
-}
