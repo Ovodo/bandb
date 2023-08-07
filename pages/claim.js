4f0884b78f60dc8fb4eb47c749453aea2fc6259d
Rob <38807144+LiquidityApe@users.noreply.github.com>
2023-08-07 07:26:12 +0100

    Update claim.js

--- pages/claim.js
+++ pages/claim.js
@@ -103,11 +103,11 @@ const Claim = (props) => {
             <p className='mt-4'>
               Nice! You can pick up{" "}
               <span className='text-teal-600 font-bold'>{dailyClaim}</span>{" "}
-              Index 📈 next time you log into BandBindex.
+              $INDEX 📈 next time you log into BandBindex.
             </p>
 
             <p className='mb-2'>
-              Get more Index on Zealy <a href='#'>Join Daily Competition.</a>
+              Get more $INDEX on Zealy <a href='#'>Join Daily Competition.</a>
             </p>
 
             <button
