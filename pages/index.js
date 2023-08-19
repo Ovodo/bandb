1b11507899cbc865dae59810261004b84b3cfd12
Rob <38807144+LiquidityApe@users.noreply.github.com>
2023-08-19 16:44:36 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -86,20 +86,28 @@ export default function Home({}) {
       </div>
     );
 
-  if (error)
-    return (
-      <div className={textTheme}>
-      <div className={` ${colorTheme} relative flex flex-col`}>
-      <div className='flex flex-row items-center h-[100vh] justify-center'>
-        <div className='max-w-max'>
-          <center>
-          <p className={textTheme}>Network Error 🌐: Kindly Reload </p>
-          <p className={textTheme}>{error.message}</p></center>
+    if (error) {
+      // Automatically reload the page after 5 seconds
+      setTimeout(() => {
+        window.location.reload();
+      }, 5000);
+    
+      return (
+        <div className={textTheme}>
+          <div className={` ${colorTheme} relative flex flex-col`}>
+            <div className='flex flex-row items-center h-[100vh] justify-center'>
+              <div className='max-w-max'>
+                <center>
+                  <p className={textTheme}>Oops! Something went wrong. We're working to fix it.</p>
+                  <p className={textTheme}>The page will automatically reload in 5 seconds.</p>
+                  {/*<p className={textTheme}>{error.message}</p>*/}
+                </center>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-      </div>
-      </div>
-    );
+      );
+    }
   return (
     <div className='flex items-center justify-center h-full flex-1'>
       <main className={` ${colorTheme} relative flex flex-col`}>
