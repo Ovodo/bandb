4d69493c84109d47ddc24322fff74905572257b4
Rob <38807144+LiquidityApe@users.noreply.github.com>
2023-08-19 15:56:06 +0100

    Update claim.js

--- pages/claim.js
+++ pages/claim.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import { useState, useEffect } from "react";
 import { boxDays } from "../data/Days";
 import Box from "../Components/Box";
