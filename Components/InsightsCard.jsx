777f981e3382c8b7d467f61e53e2e8fc953ac9ba
Rob <38807144+LiquidityApe@users.noreply.github.com>
2023-08-19 12:41:28 +0100

    Update InsightsCard.jsx

--- Components/InsightsCard.jsx
+++ Components/InsightsCard.jsx
@@ -202,7 +202,7 @@ const InsightsCard = ({
                       style={{ fontSize: 25}}
                     />
           </span>{" "}
-          {"Check out Bear and Bull Index Indicators for more information"}
+{/* This is the information icon for the tooltip, pass in the tooltip prop to change info for various components */}
           <div
             className={`absolute w-[15vw] h-[10vw] ${
               !showTooltip ? "opacity-0 hidden" : "opacity-100 flex"
